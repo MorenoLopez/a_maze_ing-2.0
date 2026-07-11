7869e61795ed5b7525ea83d7032e22feac5ee226
Moreno <127933255+MorenoLopez@users.noreply.github.com>
2026-07-11 07:16:02 +0000

    __init__

--- mazegen/__init__.py
+++ mazegen/__init__.py
@@ -4,10 +4,10 @@
 #                                                          :::      ::::::::  #
 #   __init__.py                                          :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
-#   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
+#   By: codespace <codespace@student.42.fr>          +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/28 00:00:00 by horarivo            #+#    #+#            #
-#   Updated: 2026/06/13 16:39:40 by horarivo           ###   ########.fr      #
+#   Updated: 2026/07/11 07:15:32 by codespace          ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -18,17 +18,6 @@ This file marks the ``maze/`` directory as a Python package and defines
 the public API: the names listed in ``__all__`` are the only ones that
 outside code should import. Internal modules (renderer, app, constants)
 are implementation details and are not re-exported here.
-
-Typical usage::
-
-    from mazegen import MazeGenerator, MazeSolver
-
-    gen = MazeGenerator(width=20, height=15, entry=(0, 0), exit_=(19, 14))
-    gen.generate_all()
-
-    solver = MazeSolver(gen)
-    print(solver.solve())        # ['E', 'S', 'E', ...]
-    print(solver.path_cells())   # {(0, 0), (1, 0), ...}
 """
 
 from .config import Config, die
